0b033eca447690506d2c0db01c29b9f36d1d023b
assem32 <assemhadidi74@gmail.com>
2026-07-17 18:39:53 +0300

    refactor: change endpoint name

--- src/main/java/com/auth/auth_proj/registeration/RegisterationController.java
+++ src/main/java/com/auth/auth_proj/registeration/RegisterationController.java
@@ -4,15 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.AllArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("api/v1/registeration")
-@AllArgsConstructor
+@RequestMapping("api/v1/registration")
 public class RegisterationController {
 
+    @Autowired
     private RegisterationService registerationService;
 
     @PostMapping
